596b740bf357695db572c12b251a0d4d0f94181f
shadowty1232 <tyl@null.net>
2020-08-02 17:53:41 +0100

    Initial Commit

---
+++ DPStaff/src/me/carbonate/dpstaff/DPStaff.java
@@ -0,0 +1,11 @@
+package me.carbonate.dpstaff;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DPStaff extends JavaPlugin {
+	
+	public void onEnable() {
+		getLogger().info("By Carbonate");
+	}
+
+}
